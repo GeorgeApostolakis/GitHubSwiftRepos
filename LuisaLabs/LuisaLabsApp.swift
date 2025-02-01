c784077e10ee4e9fab831039b2faa66652b4a1e7
George de Araújo Apostolakis <82823521+GeorgeApostolakis@users.noreply.github.com>
2025-02-01 20:02:36 -0300

    Initial Commit

---
+++ LuisaLabs/LuisaLabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuisaLabsApp.swift
+//  LuisaLabs
+//
+//  Created by george.apostolakis on 01/02/25.
+//
+
+import SwiftUI
+
+@main
+struct LuisaLabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
